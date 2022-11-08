f0c84ce19a060af4a968cb0c01105c3fc66d0fb8
ElBatanony <ahmedelbatanony@hotmail.com>
2022-11-08 18:54:57 +0300

    Add Screen 3

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,6 @@ import 'package:soar_quest/soar_quest.dart';
 void main() async {
   await SQApp.init("Perk Of Points",
       firebaseOptions: DefaultFirebaseOptions.currentPlatform);
-  SQApp.run([Screen("Test Screen 1"), Screen("Test Screen 2")]);
+  SQApp.run(
+      [Screen("Test Screen 1"), Screen("Test Screen 2"), Screen("Screen 3")]);
 }
